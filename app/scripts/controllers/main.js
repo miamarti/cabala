d1b2f27019e685724a24cdc410d06a54369e9a5f
miamarti <miller.augusto@gmail.com>
2015-05-09 20:26:46 -0300

    Melhorias na data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,7 @@
 angular.module('cabalaApp').controller('MainCtrl', function ($scope, KabalaBO) {
     var $core = {
         getCalculate: function () {
+            $scope.birthday = $scope.day.toString() + '/' + $scope.month.toString() + '/' + $scope.year.toString();
             $scope.kabala = null;
             $scope.kabala = (new KabalaBO($scope.name, $scope.birthday)).getCalculate();
         }

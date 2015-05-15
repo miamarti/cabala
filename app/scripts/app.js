71345a930c68f42319eb956dcf471ddadcd71228
miamarti <miller.augusto@gmail.com>
2015-05-15 20:34:37 -0300

    melhorias de usabilidade

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,19 @@
 'use strict';
 
+String.prototype.specialCharacters = function () {
+    var withAccent = 'áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ';
+    var unaccented = 'aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC';
+    var newWord = '';
+    for (var i = 0; i < this.length; i++) {
+        if (withAccent.search(this.substr(i, 1)) >= 0) {
+            newWord += unaccented.substr(withAccent.search(this.substr(i, 1)), 1);
+        } else {
+            newWord += this.substr(i, 1);
+        }
+    }
+    return newWord.toUpperCase();
+};
+
 /**
  * @ngdoc overview
  * @name cabalaApp
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,17 +16,36 @@ angular.module('cabalaApp').controller('MainCtrl', function ($scope, DictionaryB
     $scope.year = 2015;
     */
     var $core = {
+        setAnim: function (x) {
+            $('.fateContainerResult').removeClass().addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                $(this).removeClass();
+            });
+        },
         getCalculate: function () {
-			$('#cabalaApp').attr('data-status', 'show');
+            $('#cabalaApp').attr('data-status', 'show');
             $scope.birthday = $scope.day.toString() + '/' + $scope.month.toString() + '/' + $scope.year.toString();
             $scope.kabala = null;
-            $scope.kabala = (new KabalaBO($scope.name, $scope.birthday)).getCalculate();
+            $scope.kabala = (new KabalaBO($scope.name.specialCharacters(), $scope.birthday)).getCalculate();
+            $core.setAnim('bounce');
         },
         setReset: function () {
-            $scope.kabala = null;
+            $scope.kabala = false;
+            $('#cabalaApp').attr('data-status', 'edit');
+        },
+        keyUp: function () {
+            try {
+                if (($scope.name.toString().length > 5) && ($scope.day.toString().length === 2) && ($scope.month.toString().length === 2) && ($scope.year.toString().length === 4)) {
+                    $core.getCalculate();
+                } else {
+                    $core.setReset();
+                }
+            } catch (e) {
+                $core.setReset();
+            }
         }
     };
 
     $scope.getCalculate = $core.getCalculate;
     $scope.setReset = $core.setReset;
+    $scope.keyUp = $core.keyUp;
 });
